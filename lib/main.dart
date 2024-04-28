2c1e7ba9cfaf88014ac1ed056489f2d05b2c2b7e
styrix560 <60841004+styrix560@users.noreply.github.com>
2024-04-28 10:30:48 +0200

    Added afternoon

--- lib/main.dart
+++ lib/main.dart
@@ -25,17 +25,25 @@ class MainApp extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tabController = useTabController(initialLength: 2);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Benefiz Buchungs System 4",
           style: TextStyle(fontSize: 15),
         ),
+        bottom: TabBar(controller: tabController, tabs: const [
+          Tab(text: "Nachmittag"),
+          Tab(text: "Abend"),
+        ]),
       ),
-      body: const Padding(
+      body: Padding(
         padding: EdgeInsets.all(8),
-        child:
-            SingleChildScrollView(child: BookingsView(BookingTime.afternoon)),
+        child: TabBarView(controller: tabController, children: const [
+          SingleChildScrollView(child: BookingsView(BookingTime.afternoon)),
+          SingleChildScrollView(child: BookingsView(BookingTime.evening)),
+        ]),
       ),
     );
   }
