9563f68e4eb4144cbd44912c110c4c875b66db33
styrix560 <github@jakubzick.dev>
2026-03-05 12:43:30 +0100

    Feat: use proxy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import "dart:io";
+
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:flutter_hooks/flutter_hooks.dart";
 import "package:flutter_window_close/flutter_window_close.dart";
+import "package:http_proxy/http_proxy.dart";
 import "package:supernova/supernova.dart";
 
 import "api/sheets_api.dart";
@@ -31,6 +34,12 @@ Future<bool> appInitialization() async {
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final proxy = await HttpProxy.createHttpProxy();
+  if (!kDebugMode) {
+    proxy.host = "proxy.mv.schule";
+    proxy.port = "8080";
+  }
+  HttpOverrides.global = proxy;
 
   final configFound = await appInitialization();
 
